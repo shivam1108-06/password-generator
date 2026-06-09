f734c6238e98e2d64a4573fd3e773c46aaf1ed64
shivam1108-06 <shivamkotak118@gmail.com>
2026-06-09 18:28:47 +0530

    Added password generation logic

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import random
+import string
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template("index.html")
+
+    password = None
+
+    if request.method == "POST":
+
+        length = int(request.form["length"])
+
+        characters = string.ascii_letters + string.digits + string.punctuation
+
+        password = "".join(
+            random.choice(characters)
+            for _ in range(length)
+        )
+
+    return render_template(
+        "index.html",
+        password=password
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
